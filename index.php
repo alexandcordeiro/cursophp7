ba86b0a4e040a64339977528aba17072ec6e3a6a
alexandcordeiro <alexandcordeiro@gmail.com>
2018-09-18 20:20:18 -0300

    Metodos de lista classe usuario

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 
 require_once("config.php");
 
-  $root = new Usuario();
-  $root->loadById('3');
 
-  echo $root;
+//Carrega um usuario
+//  $root = new Usuario();
+//  $root->loadById('3');
+//  echo $root;
+
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search('r');
+
+//echo json_encode($search);
+
+//Carrega o usuario com login e senha
+
+  $usuario = new Usuario();
+  $usuario->login("root","456789");
+  echo $usuario;
 ?>
